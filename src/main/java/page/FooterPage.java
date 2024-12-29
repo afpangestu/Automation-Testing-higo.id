3ab21a7de644330ff2fa9f846e9bb6785b20a0a9
Aji Fauzi P <ajifauzi949@gmail.com>
2024-12-29 15:29:07 +0700

    add footer and about page

--- src/main/java/page/FooterPage.java
+++ src/main/java/page/FooterPage.java
@@ -1,4 +1,21 @@
 package page;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class FooterPage {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    // constructor
+    public void FooterPage(WebDriver driver) {
+        this.driver = driver;
+        // explicit wait
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+
 }
