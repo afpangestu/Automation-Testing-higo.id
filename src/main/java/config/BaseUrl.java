ac0e77874df5eea1d1fe6dd4e2afac0b25751e0f
Aji Fauzi P <ajifauzi949@gmail.com>
2024-12-29 11:41:54 +0700

    initial commit

---
+++ src/main/java/config/BaseUrl.java
@@ -0,0 +1,31 @@
+package config;
+
+public class BaseUrl {
+
+    // Base Url
+    public static String baseUrl = "https://higo.id";
+
+    // Header Navigation
+    public static String higo_logo = "https://higo.id";
+    public static String blog = "https://blog.higo.id";
+    public static String about_us = baseUrl + "/about-us";
+    public static String case_study = baseUrl + "/case-study";
+    public static String digital_reports = baseUrl + "/annualreport2024";
+    public static String contact_us = baseUrl + "/contact-us";
+    public static String higo_services_wifi = baseUrl + "/wifi-advertising";
+    public static String higo_services_higospot = baseUrl + "/higospot";
+    public static String higo_services_ida = baseUrl + "/integrated-digital-agency";
+
+    // Language
+    public static String higo_lang_id = baseUrl + "/id";
+    public static String higo_lang_en = baseUrl + "/en";
+
+
+    // Footer
+    public static String higo_linkedin = "https://www.linkedin.com/company/pt-higo-fitur-indonesia";
+    public static String higo_facebook = "https://www.facebook.com/HIGOSPOTINDONESIA";
+    public static String higo_instagram = "https://www.instagram.com/higo.id";
+    public static String higo_x = "https://x.com/HIGO_spot";
+    public static String tos_id = baseUrl + "terms-of-use-higospot";
+    public static String pnp_id = baseUrl + "privacy-policy-higospot";
+}
